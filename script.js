9b56fc106483b6e8070e704e7092f0bd34fee3d7
ltowler <153555297+ltowler@users.noreply.github.com>
2025-03-17 10:58:51 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+const element = React.createElement(
+  'h1', // type
+  {
+    className: 'title',
+    onClick: () => {console.log('clicked');},
+    style: { fontFamily: 'sans-serif', color: 'rgb(255, 100, 100)'}
+  }, // props
+  'Hello World!' // children
+);
+
+const container = document.getElementById('root');
+const root = ReactDOM.createRoot(container);
+root.render(element);
+
+const container2 = document.getElementById('root2');
+const root2 = ReactDOM.createRoot(container2);
+root2.render(element);
+
+document.querySelectorAll('.button')
+  .forEach(domContainer => {
+    const messageId = parseInt(domContainer.dataset.messageId);
+
+    const root = ReactDOM.createRoot(domContainer);
+    
+    root.render(
+      React.createElement(
+        'button',
+        {
+          onClick: () => { console.log(`liked message: ${messageId}`); }
+        },
+        'Like'
+      )
+    )
+  });
